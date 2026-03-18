916ffb1cad24f03c5c3cb4875f0f1f988d55c6dd
sebastian <sebastian@sebastian-IdeaPad-1-15ADA7>
2026-03-17 17:15:02 -0700

    ordenamiento de burbuja y seleccion

--- src/main/java/com/mycompany/analisisdealgoritmos/Main.java
+++ src/main/java/com/mycompany/analisisdealgoritmos/Main.java
@@ -14,6 +14,7 @@ public class Main {
         
         int[] datosBurbuja = {64, 34, 25, 12, 22, 11, 90};
         int[] datosSeleccion = {64, 25, 12, 22, 11};
+        int[] datosInsercion = {64, 34, 25, 12, 22, 11, 90};
 
         System.out.println("--- Ordenamiento Burbuja ---");
         ordenador.burbuja(datosBurbuja);
@@ -22,6 +23,10 @@ public class Main {
         System.out.println("\n--- Ordenamiento Selección ---");
         ordenador.seleccion(datosSeleccion);
         imprimirArray(datosSeleccion);
+        
+        System.out.println("\n--- Ordenamiento Insercion ---");
+        ordenador.insercion(datosInsercion);
+        imprimirArray(datosInsercion);
     }
 
     public static void imprimirArray(int[] arr) {
